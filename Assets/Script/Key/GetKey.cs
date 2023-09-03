742784d24fe6f5f0b212f210e24926441fd06fca
橋本りく <04022005rikuriku@gmail.com>
2023-09-03 09:16:04 +0900

    修正

--- Assets/Script/Key/GetKey.cs
+++ Assets/Script/Key/GetKey.cs
@@ -6,15 +6,19 @@ public class GetKey : MonoBehaviour, Ikey
 {
     public bool IsOpen => _isOpen;
     
-    private int _openCount = 0;
+    [SerializeField]
+    [Header("何回取ったら解除するか")]
+    private int _openCount = 5;
+    
+    private int _unlockCount;
     
     private bool _isOpen = false;
 
     public void Open()
     {
-        _openCount++;
-        Debug.Log($"{_openCount}回目");
-        if (_openCount == 5)
+        _unlockCount++;
+        Debug.Log($"{_unlockCount}回目");
+        if (_unlockCount == _openCount)
         { 
             _isOpen = true;
             Debug.Log("鍵を開けた"); 
